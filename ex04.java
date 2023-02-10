4759f0ea66cc6b22eff7a0c07bf1632aff14499a
rafaelasilverio <51094173+rafaelasilverio@users.noreply.github.com>
2023-02-10 01:23:29 +0000

    Salvando exercícios 4 e 5

---
+++ ex04.java
@@ -0,0 +1,6 @@
+public class ex04 {
+    public static void main(String[] args){
+        String nome = "Rafaela Aparecida Petelin Silvério";
+        System.out.println(nome);
+    }
+}
---
+++ ex05.java
@@ -0,0 +1,7 @@
+public class ex05 {
+    public static void main(String[] args){
+        float nota1 = 8, nota2 = 6;
+        float media = (nota1 + nota2) / 2;
+        System.out.println(media);
+    }
+}
