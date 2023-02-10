ed85d7fa0681453f4171920dbaac0036ae86c745
rafaelasilverio <51094173+rafaelasilverio@users.noreply.github.com>
2023-02-10 00:59:45 +0000

    Salvando exercícios

---
+++ ex03.java
@@ -0,0 +1,10 @@
+public class ex03 {
+    public static void main(String[] args){
+        int contador = 0;
+
+        while(contador < 100){
+            System.out.println("Bom dia!");
+            contador++;
+        }
+    }
+}
