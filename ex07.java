00652cf3bdc0b6d1e7b37c2e0ecd266c0da560e1
rafaelasilverio <51094173+rafaelasilverio@users.noreply.github.com>
2023-02-17 00:24:01 +0000

    exercicios 7 e 8

---
+++ ex07.java
@@ -0,0 +1,13 @@
+public class ex07 {
+    public static void main(String[] args){
+        int preco = 10;
+        if (preco < 0)
+        {
+            System.out.println("Preço negativo");
+        }
+        else
+        {
+            System.out.println("Preço positivo");
+        }
+    }
+}
---
+++ ex08.java
@@ -0,0 +1,9 @@
+public class ex08 {
+    public static void main(String[]args)
+    {
+        for(int cont = 1; cont <= 100; cont++)
+        {
+            System.out.println(cont);
+        }
+    }
+}
