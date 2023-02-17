2ba73553eba93cdf81ef02810a8922ff7b4e9c3b
rafaelasilverio <51094173+rafaelasilverio@users.noreply.github.com>
2023-02-17 01:22:56 +0000

    exercicio 9

--- ex08.java
+++ ex08.java
@@ -1,7 +1,8 @@
 public class ex08 {
     public static void main(String[]args)
     {
-        for(int cont = 1; cont <= 100; cont++)
+        int tamanho = 100;
+        for(int cont = 1; cont <= tamanho; cont++)
         {
             System.out.println(cont);
         }
---
+++ ex09.java
@@ -0,0 +1,19 @@
+public class ex09 {
+    public static void main(String[] args) 
+    {
+        int n1 = 0, n2 = 1, n3, i, tamanho = 30;
+
+        System.out.println(n1);
+        System.out.println(n2);
+
+        for (i = 2; i < tamanho; ++i)
+        {
+            n3 = n1 + n2;
+            System.out.println(n3);
+            n1 = n2;
+            n2 = n3;
+        }
+
+    }
+
+}
