8b8d179b73bb9f7104b233d1a56121255792f5e9
rafaelasilverio <51094173+rafaelasilverio@users.noreply.github.com>
2023-05-19 01:23:44 +0000

    aula 13 pt2

--- aula12/ex1/Main.java
+++ aula12/ex1/Main.java
@@ -1,9 +1,28 @@
 package ex1;
 
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Midia[] lista = new Midia[10];
+        int opcao;
+
+        for(int i=0; i < 2; i++)
+        {
+            Scanner in = new Scanner(System.in);
+            System.out.println("\n Digite 1 para CD e 2 para DVD: ");
+            opcao = in.nextInt();
+
+            if (1 == opcao) {
+                lista[i] = new CD();
+            }else{
+                lista[i] = new DVD();
+                lista[i].inserirDados();;
+            }
+        }
+            for(int i=0; i < 2; i++)
+            {
+                lista[i].printDados();
+            }
     }
 }
